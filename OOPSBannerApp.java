862af3c036d2ebaf03fecf6e811424499fd23056
pjkorba1256-cmd <pj.korba1256@gmail.com>
2026-03-15 22:21:33 +0530

    UC1

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+* OOPSBannerApp UC1 - 0OPS Banner Display Application
+* This class demonstrates a simple Java application that displays the Object
+* Oriented Programming System OOPS acronym to the console.
+*
+* @author Prince 
+* @version 1.0
+*/
+
+public class OOPSBannerApp {
+	public static void main(String[] args) {
+		System.out.println("OOPS");
+	}
+}
